b20b039f51ce9b2276903d7040711e8c2bdb4ac7
Tatiana <lts221@yandex.ru>
2021-02-17 11:10:15 +0300

    Настройки axios

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
-const instance = axios.create({});
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  // qvery параметры, которые должны передаваться с каждым запросом
+  params: {
+    apikey: process.env.VUE_APP_API_KEY,
+    plot: "full" // для получения полной информации о фильме
+  },
+});
 
 export default instance;
